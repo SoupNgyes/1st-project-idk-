eec2ef4a137aaad7ec183142c4dde4ba3ced12c9
fruitcake <80382564+SoupNgyes@users.noreply.github.com>
2022-06-06 15:56:05 +0000

    eggman's twitter announcement madlibs

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-num = input("put the number in the bag: ")
-if int(num) < 7:
-  print("This number isn't valuable enough to steal >:(")
-else: print("I SHALL BE STEALING THIS NUMBER")
+name = input("give me a name:")
+adjective = input("give me an adjective:")
+noun = input("give me a noun: ")
+verb = input("give me a verb: ")
+insult = input("give me an insult: ")
+action = input("give me an action: ")
+object = input("give me an object: ")
+ 
+
+print(f"I've come to make an announcement: {name}'s a(n) {insult}, they {action} with my {object}. Thats right, they took their {object} and he {action}")
