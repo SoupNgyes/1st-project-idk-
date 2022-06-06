04a453c866fa56dc335df776a9b7d98b5ecc7388
fruitcake <80382564+SoupNgyes@users.noreply.github.com>
2022-06-06 15:04:26 +0000

    number stuff idk

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("bye world")
+num = input("put the number in the bag: ")
+if int(num) < 7:
+  print("This number isn't valuable enough to steal >:(")
+else: print("I SHALL BE STEALING THIS NUMBER")
