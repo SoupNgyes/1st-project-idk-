b595734d2667cabe91cabb298b5bd534effc436a
fruitcake <80382564+SoupNgyes@users.noreply.github.com>
2022-06-07 01:32:00 +0000

    fixed some errors

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-name = input("give me a name:")
-adjective = input("give me an adjective:")
-noun = input("give me a noun: ")
-verb = input("give me a verb: ")
+name = input("give me a name: ")
 insult = input("give me an insult: ")
 action = input("give me an action: ")
 object = input("give me an object: ")
+object2 = input("give me another object: ")
  
 
-print(f"I've come to make an announcement: {name}'s a(n) {insult}, they {action} with my {object}. Thats right, they took their {object} and he {action}")
+print(f"I've come to make an announcement: {name}'s a(n) {insult}, they {action} on my {object}. Thats right, they took their {object2} and they {action} on my {object}.")
